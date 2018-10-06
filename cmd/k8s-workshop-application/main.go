b550a53dd8e86d7c9e8081a169757b79d0d74e30
Rangel Ivanov <rangelivanov88@gmail.com>
2018-10-06 12:07:25 +0300

    Add env vars for configuring ports

--- cmd/k8s-workshop-application/main.go
+++ cmd/k8s-workshop-application/main.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	"github.com/ironsteel/k8s-workshop-application/internal/diagnostics"
 
 	"github.com/gorilla/mux"
-	"github.com/ironsteel/k8s-workshop-application/internal/diagnostics"
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
@@ -15,19 +17,29 @@ func hello(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	log.SetFlags(log.Lshortfile | log.LstdFlags)
-	log.Print("hello k8s!")
+	log.Print("Server starting...")
+
+	port := os.Getenv("PORT")
+	diagPort := os.Getenv("DIAG_PORT")
+
+	if len(port) == 0 {
+		log.Fatal("The application PORT must be set")
+	}
+	if len(diagPort) == 0 {
+		log.Fatal("The diagnostics DIAG_PORT must be set")
+	}
 
 	router := mux.NewRouter()
 	router.HandleFunc("/", hello)
 	router.HandleFunc("/healthz", hello)
 
 	go func() {
-		if err := http.ListenAndServe(":8085", diagnostics.New()); err != nil {
+		if err := http.ListenAndServe(":"+diagPort, diagnostics.New()); err != nil {
 			log.Fatal(err)
 		}
 	}()
 
-	if err := http.ListenAndServe(":8080", router); err != nil {
+	if err := http.ListenAndServe(":"+port, router); err != nil {
 		log.Fatal(err)
 	}
 

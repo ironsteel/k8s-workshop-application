e9daa3694f0d935b89ee0c167dadc57237f44b9a
Rangel Ivanov <rangelivanov88@gmail.com>
2018-10-06 13:39:38 +0300

    Implement graceful shutdown

--- cmd/k8s-workshop-application/main.go
+++ cmd/k8s-workshop-application/main.go
@@ -1,53 +1,91 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
-
-	"github.com/ironsteel/k8s-workshop-application/internal/diagnostics"
+	"time"
 
 	"github.com/gorilla/mux"
+	"github.com/ironsteel/k8s-workshop-application/internal/diagnostics"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, http.StatusText(http.StatusOK))
-	log.Println("Hello handler was called")
+type serverConf struct {
+	port   string
+	router http.Handler
+	name   string
 }
 
 func main() {
-	log.SetFlags(log.Lshortfile | log.LstdFlags)
-	log.Print("Server starting...")
+	log.Print("Starting the application...")
 
-	port := os.Getenv("PORT")
-	diagPort := os.Getenv("DIAG_PORT")
-
-	if len(port) == 0 {
-		log.Fatal("The application PORT must be set")
+	blPort := os.Getenv("PORT")
+	if len(blPort) == 0 {
+		log.Fatal("The application port should be set")
 	}
+
+	diagPort := os.Getenv("DIAG_PORT")
 	if len(diagPort) == 0 {
-		log.Fatal("The diagnostics DIAG_PORT must be set")
+		log.Fatal("The diagnostics port should be set")
 	}
 
 	router := mux.NewRouter()
 	router.HandleFunc("/", hello)
-	router.HandleFunc("/healthz", hello)
 
-	go func() {
-		log.Print("Diagnostics server is praparing...")
-		server := &http.Server{
-			Addr:    ":" + diagPort,
-			Handler: diagnostics.New(),
-		}
-		if err := server.ListenAndServe(); err != nil {
-			log.Fatal(err)
-		}
-	}()
+	diagnostics := diagnostics.New()
 
-	log.Print("Application server is praparing...")
-	if err := http.ListenAndServe(":"+port, router); err != nil {
-		log.Fatal(err)
+	possibleErrors := make(chan error, 2)
+
+	configurations := []serverConf{
+		{
+			port:   blPort,
+			router: router,
+			name:   "application server",
+		},
+		{
+
+			port:   diagPort,
+			router: diagnostics,
+			name:   "diagnostics server",
+		},
 	}
 
+	servers := make([]*http.Server, 2)
+
+	for i, c := range configurations {
+		go func(conf serverConf, i int) {
+			log.Printf("The %s is preparing to handle connections...", conf.name)
+			servers[i] = &http.Server{
+				Addr:    ":" + conf.port,
+				Handler: conf.router,
+			}
+			err := servers[i].ListenAndServe()
+			if err != nil {
+				possibleErrors <- err
+			}
+		}(c, i)
+	}
+
+	select {
+	case err := <-possibleErrors:
+		for _, s := range servers {
+			timeout := 5 * time.Second
+			log.Printf("\nShutdown with timeout: %s\n", timeout)
+			ctx, cancel := context.WithTimeout(context.Background(), timeout)
+			defer cancel()
+			customError := s.Shutdown(ctx)
+			if customError != nil {
+				fmt.Println(customError)
+			}
+			log.Printf("Server gracefully stopped")
+		}
+		log.Fatal(err)
+	}
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	log.Print("The hello handler was called")
+	fmt.Fprint(w, http.StatusText(http.StatusOK))
 }
